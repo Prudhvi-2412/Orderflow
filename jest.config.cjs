f507d4f3c5bc635ff98b8ca90c84196b2f00d06d
Prudhvi-2412 <prudhvi16321@gmail.com>
2026-08-14 02:33:55 +0530

    updates

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,17 +1,1 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1'
-  },
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-        isolatedModules: true
-      }
-    ]
-  }
-};
+// Deprecated: Using jest.config.js with explicit --config flag in package.json
